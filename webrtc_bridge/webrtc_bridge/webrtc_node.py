e06ad60456cbff38e0663465948be0ad95534d70
Andreas Bresser <self@andreasbresser.de>
2025-06-29 14:42:52 +0200

    fix and improve devcontainer setup

--- webrtc_bridge/webrtc_bridge/webrtc_node.py
+++ webrtc_bridge/webrtc_bridge/webrtc_node.py
@@ -2,14 +2,15 @@
 """Receive WebRTC messages and re-publish as ROS 2 msgs."""
 import asyncio
 
-import cv2
-
-import numpy as np
-from aiortc import RTCPeerConnection, RTCSessionDescription, MediaStreamTrack
+from aiortc import MediaStreamTrack, RTCPeerConnection
 from aiortc.contrib.signaling import TcpSocketSignaling
+
 from av import VideoFrame
 
+import numpy as np
+
 import rclpy
+
 from rclpy.node import Node
 
 from cv_bridge import CvBridge
@@ -17,7 +18,10 @@ from sensor_msgs.msg import Image
 
 
 class WebRTCBridge(Node):
+    """ROS 2 node to provide images from an WebRTC client."""
+
     def __init__(self):
+        """Set up ROS 2 image publisher."""
         super().__init__('webrtc_bridge')
         # TODO: publish ros2 messages from webrtc
         self.get_logger().info('ROS2 node started')
@@ -26,6 +30,7 @@ class WebRTCBridge(Node):
         self.img_publisher = self.create_publisher(Image, 'image_raw', 1)
 
     async def spin(self):
+        """Update ROS 2 loop utilizing asyncio."""
         # based on https://github.com/m2-farzan/ros2-asyncio/tree/main
         while rclpy.ok():
             rclpy.spin_once(self, timeout_sec=0)
@@ -33,22 +38,22 @@ class WebRTCBridge(Node):
         self.get_logger().info('not spinning anymore')
 
     async def handle_track(self, track):
-        self.get_logger().info("Inside handle track")
+        self.get_logger().info('Inside handle track.')
         self.track = track
         frame_count = 0
         while True:
             try:
-                # self.get_logger().info("Waiting for frame...")
+                # self.get_logger().info('Waiting for frame...')
                 frame = await asyncio.wait_for(track.recv(), timeout=5.0)
                 frame_count += 1
-                # self.get_logger().info(f"Received frame {frame_count}")
+                # self.get_logger().info(f'Received frame {frame_count}')
                 if isinstance(frame, VideoFrame):
-                    frame = frame.to_ndarray(format="rgb24")
+                    frame = frame.to_ndarray(format='rgb24')
                 elif isinstance(frame, np.ndarray):
-                    self.get_logger().info("Frame type: numpy array")
+                    self.get_logger().info('Frame type: numpy array')
                 else:
                     self.get_logger().info(
-                        f"Unexpected frame type: {type(frame)}")
+                        f'Unexpected frame type: {type(frame)}')
                     continue
                 # publish as ROS message
                 img = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
@@ -57,89 +62,91 @@ class WebRTCBridge(Node):
                     f'Published video frame {frame_count} '
                     f'{frame.shape[1]}:{frame.shape[0]}')
             except asyncio.TimeoutError:
-                self.get_logger().info("Timeout waiting for frame, continuing...")
+                self.get_logger().info('Timeout waiting for frame, continuing...')
             except Exception as e:
-                self.get_logger().info(f"Error in handle_track: {str(e)}")
-                if "Connection" in str(e) or str(e) == '':
+                self.get_logger().info(f'Error in handle_track: {str(e)}')
+                if 'Connection' in str(e) or str(e) == '':
                     break
-        self.get_logger().info("Exiting handle_track")
+        self.get_logger().info('Exiting handle_track')
 
     async def run_signal(self, pc, signaling):
         await signaling.connect()
 
-        @pc.on("track")
+        @pc.on('track')
         def on_track(track):
             if isinstance(track, MediaStreamTrack):
-                self.get_logger().info(f"Receiving {track.kind} track")
+                self.get_logger().info(f'Receiving {track.kind} track')
                 asyncio.ensure_future(self.handle_track(track))
 
-        @pc.on("datachannel")
+        @pc.on('datachannel')
         def on_datachannel(channel):
-            self.get_logger().info(f"Data channel established: {channel.label}")
+            self.get_logger().info(f'Data channel established: {channel.label}')
 
-        @pc.on("connectionstatechange")
+        @pc.on('connectionstatechange')
         async def on_connectionstatechange():
-            self.get_logger().info(f"Connection state is {pc.connectionState}")
-            if pc.connectionState == "connected":
-                self.get_logger().info("WebRTC connection established successfully")
+            self.get_logger().info(f'Connection state is {pc.connectionState}')
+            if pc.connectionState == 'connected':
+                self.get_logger().info('WebRTC connection established successfully')
 
-        self.get_logger().info("Waiting for offer from sender...")
+        self.get_logger().info('Waiting for offer from sender...')
         offer = await signaling.receive()
-        self.get_logger().info("Offer received")
+        self.get_logger().info('Offer received')
         await pc.setRemoteDescription(offer)
-        self.get_logger().info("Remote description set")
+        self.get_logger().info('Remote description set')
 
         answer = await pc.createAnswer()
-        self.get_logger().info("Answer created")
+        self.get_logger().info('Answer created')
         await pc.setLocalDescription(answer)
-        self.get_logger().info("Local description set")
+        self.get_logger().info('Local description set')
 
         await signaling.send(pc.localDescription)
-        self.get_logger().info("Answer sent to sender")
+        self.get_logger().info('Answer sent to sender')
 
-        self.get_logger().info("Waiting for connection to be established...")
-        while pc.connectionState != "connected":
+        self.get_logger().info('Waiting for connection to be established...')
+        while pc.connectionState != 'connected':
             await asyncio.sleep(0.5)
 
-        self.get_logger().info("Connection established, waiting for frames...")
+        self.get_logger().info('Connection established, waiting for frames...')
         while True:
             # Wait forever to receive frames
             await asyncio.sleep(1)
 
-        self.get_logger().info("Closing connection")
+        self.get_logger().info('Closing connection')
 
 
-async def async_main(node):
+async def async_ros(node):
     await node.spin()
     node.destroy_node()
     rclpy.shutdown()
 
 
 async def webrtc_main(node):
-    # create WebRTC signaling server using aiortc
-    signaling = TcpSocketSignaling("cam", 9999)
+    """Create WebRTC signaling server using aiortc."""
+    signaling = TcpSocketSignaling('cam', 9999)
     pc = RTCPeerConnection()
     logger = node.get_logger()
     try:
         await node.run_signal(pc, signaling)
     except Exception as e:
-        logger.info(f"Error in main: {str(e)}")
+        logger.info(f'Error in main: {str(e)}')
     finally:
-        logger.info("Closing peer connection")
+        logger.info('Closing peer connection')
         await pc.close()
 
 
-async def _async_main(args=None):
+async def async_main(args=None):
+    """Create ROS node and start WebRTC and ROS loops."""
     rclpy.init(args=None)
     node = WebRTCBridge()
     # asyncio.run(async_main(node))
     async with asyncio.TaskGroup() as tg:
         tg.create_task(webrtc_main(node))
-        tg.create_task(async_main(node))
+        tg.create_task(async_ros(node))
 
 
 def main():
-    asyncio.run(_async_main())
+    """Entrypoint to start the global async loop."""
+    asyncio.run(async_main())
 
 
 if __name__ == '__main__':
--- webrtc_sender/webrtc_sender.py
+++ webrtc_sender/webrtc_sender.py
@@ -1,18 +1,24 @@
-"""
-Get local webcam and provide its data as webrtc-client
+"""Get local webcam and provide its data as webrtc-client.
+
 Based on https://github.com/eknathmali/
 Real-Time-Video-Streaming-with-WebRTC-and-Python.git
 """
 import asyncio
-import cv2
+import fractions
+
 from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
 from aiortc.contrib.signaling import TcpSocketSignaling
+
 from av import VideoFrame
-import fractions
+
+import cv2
 
 
 class CustomVideoStreamTrack(VideoStreamTrack):
-    def __init__(self, camera_id):
+    """Provide Video Stream from Camera using cv2."""
+
+    def __init__(self, camera_id: int):
+        """Set up video capture and reset frame count."""
         super().__init__()
         self.cap = cv2.VideoCapture(camera_id)
         # TODO: add ROS 2 messages to configure frame rate and resolution.
@@ -21,18 +27,25 @@ class CustomVideoStreamTrack(VideoStreamTrack):
         self.frame_count = 0
 
     async def recv(self):
+        """
+        Get frame from caputre device and return.
+
+        The name "rcev" for receive might be a bit misleading, as we
+        are not receiving images but instead provide the next frame.
+        """
         self.frame_count += 1
         ret, frame = self.cap.read()
         if not ret:
             print('Failed to read frame from camera.')
             return None
-        video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
+        video_frame = VideoFrame.from_ndarray(frame, format='rgb24')
         video_frame.pts = self.frame_count
         video_frame.time_base = fractions.Fraction(1, 30)
         return video_frame
 
 
-async def setup_webrtc_and_run(host, port, camera_id):
+async def setup_webrtc_and_run(host: str, port: int, camera_id: int):
+    """Set up TcpSocketSignaling server and our custom VideoStreamTrack."""
     signaling = TcpSocketSignaling(host, port)
     pc = RTCPeerConnection()
     video_sender = CustomVideoStreamTrack(camera_id)
@@ -41,13 +54,13 @@ async def setup_webrtc_and_run(host, port, camera_id):
     try:
         await signaling.connect()
 
-        @pc.on("datachannel")
+        @pc.on('datachannel')
         def on_datachannel(channel):
-            print(f"Data channel established: {channel.label}")
+            print(f'Data channel established: {channel.label}')
 
-        @pc.on("connectionstatechange")
+        @pc.on('connectionstatechange')
         async def on_connectionstatechange():
-            print(f"Connection state is {pc.connectionState}")
+            print(f'Connection state is {pc.connectionState}')
 
         offer = await pc.createOffer()
         await pc.setLocalDescription(offer)
@@ -57,22 +70,23 @@ async def setup_webrtc_and_run(host, port, camera_id):
             obj = await signaling.receive()
             if isinstance(obj, RTCSessionDescription):
                 await pc.setRemoteDescription(obj)
-                print("Remote description set")
+                print('Remote description set')
             elif obj is None:
-                print("Signaling ended")
+                print('Signaling ended')
                 break
-        print("Closing connection")
+        print('Closing connection')
     finally:
         await pc.close()
 
 
 async def main():
+    """Connect to server and provide Camera images."""
     # TODO: get address and port of some server
-    host = "0.0.0.0"
+    host = '0.0.0.0'
     port = 9999
     camera_id = 0
     while True:
         await setup_webrtc_and_run(host, port, camera_id)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
